336af74406fc14ec242cd55f40b350f9d010a827
Mattia Fussi <mattia.fussi@iit.it>
2022-01-12 11:39:56 +0100

    Added four bar assets and steps 1,2,3

---
+++ init.m
@@ -0,0 +1,10 @@
+%% Data initialization script
+
+% Electrical parameters
+R = 19.8; % ohm
+L = 250; % uH
+Vcc = 12; % V
+% Motor parameters
+Kt = 11.4; % mNm/A
+bm = 1.3e-5; % mNm/rpm
+Jm = 0.65; % gcm2
